cebb0c51b78c7123d84b8cc1dec3739b180895ba
Diego Jasso <gummyco@gmail.com>
2015-11-17 11:01:42 -0600

    Initial Commit

---
+++ Pawnbot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pawnbot
+//
+//  Created by Diego Sánchez on 11/17/15.
+//  Copyright © 2015 Pademobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
